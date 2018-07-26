cfcebb37a6c989bee35f36fdc4684ed0a24543b2
David Garcia Perez <davidgarcia@Davids-MacBook-Pro-2.local>
2018-07-26 22:45:59 +0100

    feat(currencyApp): Included components and services in module.

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { WebSocketService } from './services/webSocket.service';
 import { AppComponent } from './app.component';
+import { CurrencyPanelComponent } from './components/currencyPanel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrencyPanelComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    WebSocketService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
