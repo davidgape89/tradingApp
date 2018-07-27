be3c3776d2d1d246ee111f09b5e659ca19acbc0c
David Garcia Perez <davidgarcia@Davids-MacBook-Pro-2.local>
2018-07-27 15:04:52 +0100

    feat(currencyApp): Styled price tag component.

--- frontEnd/src/app/components/priceTag/priceTag.component.ts
+++ frontEnd/src/app/components/priceTag/priceTag.component.ts
@@ -5,18 +5,26 @@ import {Component, OnInit, Input} from '@angular/core';
     templateUrl: './priceTag.component.html',
     styleUrls: ['./priceTag.component.scss']
 })
-export class PriceTagComponent implements OnInit {
+export class PriceTagComponent {
     @Input() price: number;
     @Input() currency: string;
     @Input() action: string;
 
-    get priceString(): string {return this.price.toFixed(4)}
+    get priceString(): string {return this.price.toFixed(5).toString()}
+    get priceStringLength(): number {return this.priceString.length};
+
+    get priceFirstPart(): string {
+        return this.priceString.substring(0, this.priceStringLength-3)
+    }
+    get priceMiddlePart(): string {
+        return this.priceString.substring(this.priceStringLength-3, this.priceStringLength-1)
+    }
+    get priceLastPart(): string {
+        return this.priceString.substring(this.priceStringLength-1, this.priceStringLength)
+    }
 
     constructor() { 
     }
 
-    ngOnInit() {
-    
-    }
 
 }
